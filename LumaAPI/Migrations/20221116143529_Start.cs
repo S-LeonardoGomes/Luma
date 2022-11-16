8202247bc68a2679cf93a12ed7986419060feada
Leonardo Gomes <leonardo.nick.santos@gmail.com>
2022-11-16 11:36:23 -0300

    Adição de migration para criação de tabela no banco de dados.

---
+++ LumaAPI/Migrations/20221116143529_Start.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LumaEventService.Migrations
+{
+    public partial class Start : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LumaEvents",
+                columns: table => new
+                {
+                    EventId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EventUtcDateStart = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EventUtcDateEnd = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LumaEvents", x => x.EventId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LumaEvents");
+        }
+    }
+}
